2e520a34bc653bbdd6c422c6a603f93927c17204
Taylor Friesen <taylorfriesen@protonmail.com>
2021-01-28 14:34:02 -0800

    task 6 complete

--- index.js
+++ index.js
@@ -204,8 +204,18 @@ class Instructor extends Lambdasian {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+     constructor(attr) {
+       super(attr);
+       this.gradClassName = attr.gradClassName;
+       this.favInstructor = attr.favInstructor;
+     }
+     standUp (channel) {
+       return `${this.name} announces to ${channel}, @channel standy times!`;
+     }
+     debugsCode(student, subject) {
+       return `${this.name} debugs ${student.name}'s code on ${subject}`;
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
